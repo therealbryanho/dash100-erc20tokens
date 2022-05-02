ba5bb25079891763427ced42574e9b451b2ac4d6
therealbryanho <therealbryanho@gmail.com>
2022-05-02 20:30:25 +0800

    first commit

---
+++ BHT.sol
@@ -0,0 +1,13 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract BRYANHO_TOKEN is ERC20, ERC20Burnable {
+    constructor() ERC20("BRYANHO TOKEN", "BHT") {
+        _mint(msg.sender, 100_000_000_000 * 10**18 );
+    }
+}
